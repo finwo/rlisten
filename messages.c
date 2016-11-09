8e99cffc76bfbc0a7e468009b748d4ae0669c899
Robin Bron <robin@finwo.nl>
2016-11-09 20:49:29 +0100

    Removed useless whitespace

--- messages.c
+++ messages.c
@@ -31,25 +31,25 @@ void message_usage(char *programName)
 
 void message_license()
 {
-  printf("\n");  
-  printf("MIT LICENSE\n");  
-  printf("\n");  
-  printf("Copyright (c) 2016 Finwo\n");  
-  printf("\n");  
-  printf("Permission is hereby granted, free of charge, to any person obtaining a copy\n");  
-  printf("of this software and associated documentation files (the \"Software\"), to deal\n");  
-  printf("in the Software without restriction, including without limitation the rights\n");  
-  printf("to use, copy, modify, merge, publish, distribute, sublicense, and/or sell\n");  
-  printf("copies of the Software, and to permit persons to whom the Software is\n");  
-  printf("furnished to do so, subject to the following conditions:\n");  
-  printf("\n");  
-  printf("The above copyright notice and this permission notice shall be included in all\n");  
-  printf("copies or substantial portions of the Software.\n");  
-  printf("\n");  
-  printf("THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\n");  
-  printf("IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\n");  
-  printf("FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\n");  
-  printf("AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\n");  
+  printf("\n");
+  printf("MIT LICENSE\n");
+  printf("\n");
+  printf("Copyright (c) 2016 finwo\n");
+  printf("\n");
+  printf("Permission is hereby granted, free of charge, to any person obtaining a copy\n");
+  printf("of this software and associated documentation files (the \"Software\"), to deal\n");
+  printf("in the Software without restriction, including without limitation the rights\n");
+  printf("to use, copy, modify, merge, publish, distribute, sublicense, and/or sell\n");
+  printf("copies of the Software, and to permit persons to whom the Software is\n");
+  printf("furnished to do so, subject to the following conditions:\n");
+  printf("\n");
+  printf("The above copyright notice and this permission notice shall be included in all\n");
+  printf("copies or substantial portions of the Software.\n");
+  printf("\n");
+  printf("THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\n");
+  printf("IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\n");
+  printf("FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\n");
+  printf("AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\n");
   printf("LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\n");
   printf("OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE\n");
   printf("SOFTWARE.\n");
